4b0dcbf755f289c422841e40ddbc40af289d982a
alexraduca01 <alex.raduca01@icloud.com>
2023-11-13 17:04:52 +0100

    added LoaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <HeaderComponent />
-  <div class="bg-warning">
-    <div class="container p-4">
-      <select name="cards" id="cards" class="form-select p-1">
-        <FilterComponent v-for="item in myCards" :archetype="item.archetype" />
-      </select>
-    </div>
-    <div class="container bg-white p-5">
-      <div class="bg-primary p-2 row">
-        <h5 class="m-0">Found {{ myCards.length }} cards</h5>
+  <LoaderComponent v-if="myCards.length < 100" />
+  <div v-else>
+    <HeaderComponent />
+    <div class="bg-warning">
+      <div class="container p-4">
+        <select name="cards" id="cards" class="form-select p-1">
+          <FilterComponent v-for="item in myCards" :archetype="item.archetype" />
+        </select>
       </div>
-      <div class="row justify-content-between">
-        <CardComponent v-for="card in myCards" :thumb="card.card_images[0].image_url" :title="card.name" :race="card.archetype"/>
+      <div class="container bg-white p-5">
+        <div class="bg-dark p-2 row">
+          <h5 class="m-0 text-white">Found {{ myCards.length }} cards</h5>
+        </div>
+        <div class="row justify-content-between">
+          <CardComponent v-for="card in myCards" :thumb="card.card_images[0].image_url" :title="card.name" :race="card.archetype"/>
+        </div>
       </div>
     </div>
   </div>
+  
   
   
 </template>
@@ -24,6 +28,7 @@
 import CardComponent from './components/CardComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import FilterComponent from './components/FilterComponent.vue';
+import LoaderComponent from './components/LoaderComponent.vue';
 import axios from 'axios';
 import {store} from './data/store';
   export default {
@@ -31,6 +36,7 @@ import {store} from './data/store';
       CardComponent,
       HeaderComponent,
       FilterComponent,
+      LoaderComponent,
     },
     data(){
       return {
---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2 class="text-white">Attendi il caricamento delle carte</h2>
+        <img src="https://www.staynerd.com/wp-content/uploads/yu-gi-oh-il-lato-oscuro-delle-dimensioni.jpg" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LoaderComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+img {
+    width: 100%;
+}
+h2 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
